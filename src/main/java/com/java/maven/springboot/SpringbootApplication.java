febb668aee451ce8b25fb1b5e86a8418a8665ebd
alexkkm <alexkong0222@gmail.com>
2022-08-15 17:37:21 +0800

    initial setup for Springboot Application

---
+++ src/main/java/com/java/maven/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.java.maven.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// SpringBoot Application is the main class that contains the main method of the application
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
